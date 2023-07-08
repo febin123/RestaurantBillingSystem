77791c37208adc6258967b22edbf82a15ae545f5
febin123 <febsep96@gmail.com>
2023-07-08 18:21:50 +0530

    billing backend node server connection established

---
+++ server.js
@@ -0,0 +1,32 @@
+const express=require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors=require('cors')
+const dotanv = require('dotenv')
+require('colors')
+
+//dotenv config
+dotanv.config()
+
+//rest object
+const app=express()
+
+//middleware
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended:false }))
+app.use(morgan("dev"))
+
+//routes
+app.get('/',(req,res)=>{
+    res.send("<h1>Billing System BACKEND</h1>")
+});
+
+//port
+const PORT=process.env.PORT || 8080;
+
+//listen
+app.listen(PORT,()=>{
+    console.log(`Server listening on port ${PORT}`.bgCyan.white)
+})
