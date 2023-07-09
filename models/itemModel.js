103c6c24c7646dbdef4c66729266484c1cee3196
febin123 <febsep96@gmail.com>
2023-07-09 17:37:13 +0530

    mongodb connected created

---
+++ models/itemModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const itemSchema =mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    }
+})
