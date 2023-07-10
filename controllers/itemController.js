b7812fdc61c7c4863d61b3315d28f878b3dd8d54
febin123 <febsep96@gmail.com>
2023-07-10 06:37:09 +0530

    created item controller function

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,1 +1,10 @@
-export const getItemController =()=>{}
+const itemModel=require('../models/itemModel')
+
+export const getItemController =async(req,res)=>{
+    try{
+        const items=await itemModel.find()
+        res.status(200).send(items)
+    }catch(error){
+        console.error(error)
+    }
+}
