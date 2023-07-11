8c3c9be8277841df0df2d1ee65b20148d8dc3d7c
febin123 <febsep96@gmail.com>
2023-07-11 07:19:58 +0530

    structed navbar

--- billingsystem/src/components/DefaultLayout.js
+++ billingsystem/src/components/DefaultLayout.js
@@ -1,77 +1,80 @@
+import React from "react";
+import { Layout, Menu } from "antd";
+import { Link } from "react-router-dom";
 import {
-    MenuFoldOutlined,
-    UploadOutlined,
-    UserOutlined,
-    VideoCameraOutlined,
-  } from '@ant-design/icons';
-  import React from 'react'
-  import { Button, Layout, Menu, theme } from 'antd';
-  import { useState } from 'react';
-  import "../styles/DefaultLayout.css"
-//   import DefaultLayout from './DefaultLayout';
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  LogoutOutlined,
+  HomeOutlined,
+  CopyOutlined,
+  UnorderedListOutlined,
+} from "@ant-design/icons";
+import "../styles/DefaultLayout.css";
 const { Header, Sider, Content } = Layout;
-  const DefaultLayout = () => {
-    const [collapsed, setCollapsed] = useState(false);
-    const {
-      token: { colorBgContainer },
-    } = theme.useToken();
+
+export default class DefaultLayout extends React.Component {
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
+  render() {
     return (
       <Layout>
-        <Sider trigger={null} collapsible collapsed={collapsed}>
-          <div className="demo-logo-vertical" />
+        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+          <div className="logo">
+            <h4 className="text-center text-light font-wight-bold mt-4">Restaurant Billing System</h4>
+          </div>
           <Menu
             theme="dark"
             mode="inline"
-            defaultSelectedKeys={['1']}
-            items={[
-              {
-                key: '1',
-                icon: <UserOutlined />,
-                label: 'nav 1',
-              },
-              {
-                key: '2',
-                icon: <VideoCameraOutlined />,
-                label: 'nav 2',
-              },
-              {
-                key: '3',
-                icon: <UploadOutlined />,
-                label: 'nav 3',
-              },
-            ]}
-          />
-        </Sider>
-        <Layout>
-          <Header
-            style={{
-              padding: 0,
-              background: colorBgContainer,
-            }}
+            defaultSelectedKeys={window.location.pathname}
           >
-            <Button
-              type="text"
-              icon={collapsed ? <MenuFoldOutlined /> : <MenuFoldOutlined />}
-              onClick={() => setCollapsed(!collapsed)}
-              style={{
-                fontSize: '16px',
-                width: 64,
-                height: 64,
-              }}
-            />
+            <Menu.Item key="/" icon={<HomeOutlined />}>
+              <Link to="/">Home</Link>
+            </Menu.Item>
+            <Menu.Item key="/bills" icon={<CopyOutlined />}>
+              <Link to="/bills">Bills</Link>
+            </Menu.Item>
+            <Menu.Item key="/items" icon={<UnorderedListOutlined />}>
+              <Link to="/items">Items</Link>
+            </Menu.Item>
+            <Menu.Item key="/customers" icon={<UserOutlined />}>
+              <Link to="/customers">Cutomers</Link>
+            </Menu.Item>
+            <Menu.Item key="/logout" icon={<LogoutOutlined />}>
+              Logout
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            {React.createElement(
+              this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+              {
+                className: "trigger",
+                onClick: this.toggle,
+              }
+            )}
           </Header>
           <Content
+            className="site-layout-background"
             style={{
-              margin: '24px 16px',
+              margin: "24px 16px",
               padding: 24,
               minHeight: 280,
-              background: colorBgContainer,
             }}
           >
-            Content
+            {this.props.children}
           </Content>
         </Layout>
       </Layout>
     );
-  };
-  export default DefaultLayout;
+  }
+}
