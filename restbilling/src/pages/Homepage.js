4c06ecd5f78c2c5afee25b89ffc8c80016310f5e
febin123 <febsep96@gmail.com>
2023-08-08 21:39:57 +0530

    created categories array

--- restbilling/src/pages/Homepage.js
+++ restbilling/src/pages/Homepage.js
@@ -10,6 +10,21 @@ const Homepage = () => {
 
     const[itemsData,setItemsData]=useState([])
 
+    const[selectedCategory,setSelectedCategory]=useState('Soft Drinks')
+    const categories=[
+      {
+        name:'Soft drinks',
+        imageUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZcXBffUKO4BvtATUXlCV2JcjvElklB7wQ4t1Wk8vxVQ&s'
+      },
+      {
+        name:'Curry',
+        imageUrl:'https://www.simplyrecipes.com/thmb/-QzmQynep4nIQ3tncO0v3_xpPd0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__01__Butter-Chicken-LEAD-2-6ca76f24bbe74114a09958073cb9c76f.jpg'
+      },
+      {
+        name:'Rice',
+        imageUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDI2GVHXL_WNzLsH9Tx97KIpXmE9UN1IuIl8leMzisqp-3lei7IWVB_JuxXHY9YPb1iE4&usqp=CAU'
+      }
+    ]
     const dispatch=useDispatch()
 
     //useEffect
