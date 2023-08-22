b7798141dcbe31e721c3db054fbddcd692501574
febin123 <febsep96@gmail.com>
2023-08-22 23:38:23 +0530

    created Customers page

---
+++ restbilling/src/pages/Customers.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import DefaultLayout from '../components/DefaultLayout'
+function Customers() {
+  return (
+    <div>
+      <DefaultLayout>
+        Customers
+      </DefaultLayout>
+    </div>
+  )
+}
+
+export default Customers
