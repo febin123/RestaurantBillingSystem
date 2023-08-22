c7c8af256ab59ad37069b16bce41b4a504976f08
febin123 <febsep96@gmail.com>
2023-08-22 23:37:34 +0530

    created Bills page

---
+++ restbilling/src/pages/Bills.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DefaultLayout from './../components/DefaultLayout';
+
+function Bills() {
+  return (
+    <div>
+      <DefaultLayout>Bills</DefaultLayout>
+    </div>
+  )
+}
+
+export default Bills
